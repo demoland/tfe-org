f387131abf36808a41cf3fd667a80f3069a9f6a4
Dan Fedick <daniel.fedick@hashicorp.com>
2023-03-14 13:14:20 -0400

    Add a workspace in a project

--- main.tf
+++ main.tf
@@ -3,7 +3,17 @@ resource "tfe_organization" "demodan" {
   email = "daniel.fedick@hashicorp.com"
 }
 
-resource "tfe_project" "demo_project" {
+resource "tfe_project" "demo" {
+  name         = "demo-dan"
   organization = tfe_organization.demodan.name
-  name         = "demodan"
+}
+
+resource "tfe_workspace" "aws_vpc" {
+  name         = "aws-vpc"
+  organization = tfe_organization.demodan.name
+  tag_names = [
+    "aws",
+    "vpc",
+    "terraform",
+  ]
 }

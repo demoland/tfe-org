986b2a40f752a32bf59a03963dc1a8b2871df7d3
Dan Fedick <daniel.fedick@hashicorp.com>
2023-03-14 13:35:14 -0400

    change the names

--- main.tf
+++ main.tf
@@ -4,18 +4,18 @@ resource "tfe_organization" "tfeo" {
   email = var.email
 }
 
-# resource "tfe_project" "project" {
-#   name         = var.project_name
-#   organization = tfe_organization.tfeo.name
-# }
+resource "tfe_project" "project" {
+  name         = var.project_name
+  organization = tfe_organization.tfeo.name
+}
 
-# resource "tfe_workspace" "workspace" {
-#   name         = var.workspace_name
-#   organization = tfe_organization.tfeo.name
-#   project_id = tfe_project.project.id
-#   tag_names = [
-#     "aws",
-#     "vpc",
-#     "terraform",
-#   ]
-# }
+resource "tfe_workspace" "workspace" {
+  name         = var.workspace_name
+  organization = tfe_organization.tfeo.name
+  project_id = tfe_project.project.id
+  tag_names = [
+    "aws",
+    "vpc",
+    "terraform",
+  ]
+}

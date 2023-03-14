38a5be1c65a05dcc8bb2c432b4865458e218f855
Dan Fedick <daniel.fedick@hashicorp.com>
2023-03-14 13:32:32 -0400

    org

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
+
 resource "tfe_organization" "organization" {
   name  = var.org_name
   email = var.email
 }
 
-resource "tfe_project" "project" {
-  name         = var.project_name
-  organization = tfe_organization.organization.name
-}
+# resource "tfe_project" "project" {
+#   name         = var.project_name
+#   organization = tfe_organization.organization.name
+# }
 
-resource "tfe_workspace" "workspace" {
-  name         = var.workspace_name
-  organization = tfe_organization.organization.name
-  project_id = tfe_project.project.id
-  tag_names = [
-    "aws",
-    "vpc",
-    "terraform",
-  ]
-}
+# resource "tfe_workspace" "workspace" {
+#   name         = var.workspace_name
+#   organization = tfe_organization.organization.name
+#   project_id = tfe_project.project.id
+#   tag_names = [
+#     "aws",
+#     "vpc",
+#     "terraform",
+#   ]
+# }

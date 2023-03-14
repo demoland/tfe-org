8f358837572d8cf40a16555298091b608d4c58f8
Dan Fedick <daniel.fedick@hashicorp.com>
2023-03-14 13:26:05 -0400

    Move vars to variables.tf

--- variables.tf
+++ variables.tf
@@ -2,4 +2,23 @@ variable "token" {
   type        = string
   description = "The token to use for authentication with the Terraform Enterprise API."
 }
+variable "org_name" {
+  type = string
+  default = "ALOHA"
+}
+
+variable "email" {
+  type = string
+  default = "daniel.fedick@hashicorp.com"
+}
+
+variable "project_name" {
+  type = string
+  default = "ALOHA"
+}
+
+variable "workspace_name" {
+  type = string
+  default = "ALOHA"
+}
  

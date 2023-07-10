946fbf53dec75fb6442a18c350b817d862c10606
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-07-10 14:57:06 -0400

    Backend update

--- workspaces/backend.tf
+++ workspaces/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "remote" {
-    organization = "demo-land"
+    organization = "hashicorp-federal"
     workspaces {
-      name = "tfe-organizations"
+      name = "tfe-workspaces"
     }
   }
 }

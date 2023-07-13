39576ee7a78ae4c629cdbbd9d6dcba284cd967da
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-07-13 10:13:09 -0400

    Adding dynamic block. Will it work?

--- workspaces/main.tf
+++ workspaces/main.tf
@@ -9,18 +9,18 @@ resource "tfe_workspace" "this" {
   project_id          = local.projects[each.value.project_name]
   auto_apply          = each.value.auto_apply
   global_remote_state = each.value.global_remote_state
- # trigger_prefixes    = each.value.trigger_prefixes
+  working_directory = each.value.working_directory
+  dynamic "trigger_prefixes" {
+   for_each = each.value.trigger_prefixes != null ? list(each.value.trigger_prefixes) : []
+    content {
+      trigger_prefixes = trigger_prefixes.value
+    }
+  }
+
   vcs_repo {
     identifier     = "${each.value.vcs.org}/${each.value.vcs.repo}"
     branch         = each.value.vcs.branch
     oauth_token_id = each.value.vcs.provider == "github" ? var.github_token_id : var.gitlab_token_id
   }
-  working_directory = each.value.working_directory
 
-  dynamic "trigger_prefixes" {
-    for_each = each.value.trigger_prefixes != null ? list(each.value.trigger_prefixes) : []
-    content {
-      trigger_prefixes = trigger_prefixes.value
-    }
-  }
 }
